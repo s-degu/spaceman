936d8fabc2abf1c20edab2d467a8fb7df3585c29
s-degu <s-degu@users.noreply.github.com>
2021-10-30 13:25:17 +0900

    追加：アコーディオン

--- src/js/script.js
+++ src/js/script.js
@@ -24,7 +24,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   // ボタンの表示設定
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
+    if ($(this).scrollTop() > $('.p-hero').height()) {
       // 指定px以上のスクロールでボタンを表示
       topBtn.fadeIn();
     } else {
@@ -41,8 +41,35 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     return false;
   });
 
+  
+  //アコーディオンをクリックした時の動作
+  $('.js-accordion').on('click', function () {//タイトル要素をクリックしたら
+    $('.p-faq__data').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
 
+    var findElm = $(this).next(".p-faq__data");//タイトル直後のアコーディオンを行うエリアを取得
 
+    if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
+      $(this).removeClass('close');//クラス名を除去    
+    } else {//それ以外は
+      $('.close').removeClass('close'); //クラス名closeを全て除去した後
+      $(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+      $(findElm).slideDown(500);//アコーディオンを開く
+    }
+  });
+  //ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+  $(window).on('load', function () {
+    $('.p-faq__item:first-of-type').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+    $(".open").each(function (index, element) { //openクラスを取得
+      var Title = $(element).children('.p-faq__def-title'); //openクラスの子要素のtitleクラスを取得
+      $(Title).addClass('close'); ///タイトルにクラス名closeを付与し
+      var Box = $(element).children('.p-faq__data'); //openクラスの子要素boxクラスを取得
+      // console.log(Box);
+      $(Box).slideDown(500); //アコーディオンを開く
+      // $(Box).addClass(".test"); //アコーディオンを開く
+    });
+  });
+  
+  
   // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
   $(document).on('click', 'a[href*="#"]', function () {
     let time = 400;
@@ -53,5 +80,5 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $('html,body').animate({ scrollTop: targetY }, time, 'swing');
     return false;
   });
-
+  
 });
